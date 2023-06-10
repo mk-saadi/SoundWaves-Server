ff46c9a6849ba72c73550a2f795d100916df971f
mk-saadi <mksaadi820@gmail.com>
2023-06-10 12:14:44 +0600

    instructor

--- index.js
+++ index.js
@@ -10,11 +10,38 @@ const port = process.env.PORT || 12000;
 app.use(cors());
 app.use(express.json());
 
+// const verifyJWT = (req, res, next) => {
+//     const authorization = req.headers.authorization;
+//     if (!authorization) {
+//         return res.status(401).send({ error: true, message: "unauthorized access" });
+//     }
+//     const token = authorization.split(" ")[1];
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).send({ error: true, message: "unauthorized access" });
+//         }
+//         req.decoded = decoded;
+
+//         const userType = decoded.userType;
+//         if (userType !== "admin") {
+//             return res.status(403).send({ error: true, message: "Access forbidden" });
+//         }
+
+//         // const { userType } = req.decoded;
+//         // if (userType !== "admin" && userType !== "instructor") {
+//         //     return res.status(403).send({ error: true, message: "Access forbidden" });
+//         // }
+//         next();
+//     });
+// };
+
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (!authorization) {
         return res.status(401).send({ error: true, message: "unauthorized access" });
     }
+    // bearer token
     const token = authorization.split(" ")[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
@@ -23,10 +50,10 @@ const verifyJWT = (req, res, next) => {
         }
         req.decoded = decoded;
 
-        const userType = decoded.userType;
-        if (userType !== "admin") {
-            return res.status(403).send({ error: true, message: "Access forbidden" });
-        }
+        // const { userType } = req.decoded;
+        // if (userType !== "admin" && userType !== "instructor") {
+        //     return res.status(403).send({ error: true, message: "Access forbidden" });
+        // }
         next();
     });
 };
@@ -51,6 +78,7 @@ async function run() {
         const classCollection = client.db("summerCamp12").collection("classes");
         const userCollection = client.db("summerCamp12").collection("users");
 
+        // middleware
         app.post("/jwt", (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -60,6 +88,17 @@ async function run() {
             res.send({ token });
         });
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== "admin") {
+                return res.status(403).send({ error: true, message: "forbidden message" });
+            }
+            next();
+        };
+
+        // mongodb api
         app.get("/classes", async (req, res) => {
             const cursor = classCollection.find();
             const result = await cursor.toArray();
@@ -90,6 +129,70 @@ async function run() {
                 .catch((error) => {
                     res.status(500).send(error);
                 });
+        });
+
+        app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+            console.log(result);
+        });
+
+        app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false });
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === "admin" };
+            res.send(result);
+        });
+
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: "admin",
+                },
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        // instructor
+
+        app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: true });
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { instructor: user?.role === "instructor" };
+            res.send(result);
+        });
+
+        app.patch("/users/instructor/:id", async (req, res) => {
+            const id = req.params.id;
+
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: "instructor",
+                },
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
         });
 
         await client.db("admin").command({ ping: 1 });
